2ea066e72256b578f1809d908dd740762d85be4a
Thomas Sidesinger <55333409+tsidesinger@users.noreply.github.com>
2019-09-21 20:50:17 -0400

    Exercism.com Work

---
+++ exercism/dad/age.py
@@ -0,0 +1,9 @@
+input1 = input() 
+int_input1 = int(input1)
+agein2050 = 31 + int_input1
+agesentance = "in twenty fifty you will be " + str(agein2050) + " years old."
+
+if agein2050 < 152 :
+    print(agesentance) 
+else:
+     print ("you are definitly not that age.")
---
+++ exercism/two-fer/two_fer.py
@@ -0,0 +1,8 @@
+#def two_fer(name = ""):
+ #   if name == "":
+  #      return "One for you, one for me."
+   # else:
+    #    s = "One for " + name + ", one for me."
+     #   return s
+def two_fer(name = ""):
+'one for %s' % name 
