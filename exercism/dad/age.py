45ce4d277111752c7b3d9ff58f37b6deeb4cf5dd
Thomas Sidesinger <55333409+tsidesinger@users.noreply.github.com>
2019-09-29 10:55:55 -0400

    made this a litle more readable

--- exercism/dad/age.py
+++ exercism/dad/age.py
@@ -1,9 +1,9 @@
-input1 = input() 
-int_input1 = int(input1)
-agein2050 = 31 + int_input1
-agesentance = "in twenty fifty you will be " + str(agein2050) + " years old."
+ageInput = input() 
+age = int(ageInput)
+ageIn2050 = 31 + age
+ageSentance = "in twenty fifty you will be " + str(ageIn2050) + " years old."
 
-if agein2050 < 152 :
-    print(agesentance) 
+if ageIn2050 < 152 :
+    print(ageSentance) 
 else:
      print ("you are definitly not that age.")
