a82cb3e4f37986f2489f189052082a46c68a1d66
Thomas Sidesinger <55333409+tsidesinger@users.noreply.github.com>
2019-10-08 17:36:27 -0400

    made class and race fetures for the D&D charecter progect

--- exercism/python/dnd-character/dnd_character.py
+++ exercism/python/dnd-character/dnd_character.py
@@ -1,4 +1,5 @@
 import random
+enufrom enum import Enum
 class Character:
     def __init__(self):
         self.strength = self.ability()
@@ -20,3 +21,32 @@ class Character:
 
 def modifier(amt):
     return (amt - 10)//2 
+isRandom = 0
+
+class consoleClass: 
+     print("Would you like a random charecter? y or n")
+     randomAnswer = input()
+     if randomAnswer == "y":
+        isRandom = 1
+     else:
+         print("please choose a race 1) Elf 2) Human 3) Dwarf 4) Hafling 5) Dragonborn 6) Halfelf 7) Halfork 8) Tiefling")
+         class Race(enum)
+            Elf = 1
+            Human = 2
+            Dwarf = 3
+            Halfling = 4
+            Dragonborn = 5
+            Halfelf = 6
+            Halfork = 7
+            Tiefling = 8
+         print("please chosse a class 1) Barbarian 2) Bard 3) Cleric 4) Druid 5) Fighter 6) paladin 7) Ranger 8) Rouge 9) Wizard")
+         class characterClass(enum)
+             Barbarian = 1
+             Bard =2
+             Cleric =3
+             Druid =4
+             Fighter = 5
+             paladin = 6
+             Ranger = 7 
+             Rouge = 8
+             Wizard = 9
